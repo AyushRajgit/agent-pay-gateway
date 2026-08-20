fdc1910a34b89d811ff9cb2265a9cd275bdc9007
AyushRajgit <ayushraj2450@gmail.com>
2026-08-20 21:47:12 +0530

    Initial SpringBoot setup, Postgresql communication, razorpay test mode integration done

---
+++ agent-pay-gateway/src/main/java/com/razorpay/buildathon/agent_pay_gateway/AgentPayGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.razorpay.buildathon.agent_pay_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentPayGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentPayGatewayApplication.class, args);
+
+	}
+
+}
---
+++ agent-pay-gateway/src/test/java/com/razorpay/buildathon/agent_pay_gateway/AgentPayGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razorpay.buildathon.agent_pay_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentPayGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
